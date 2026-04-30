a1603ef1d0989646746efdd9ba3269ac9bda4441
Mayank Rajput <mayankrajput9576@gmail.com>
2026-05-01 03:01:24 +0530

    fix frontend pri

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-
-  plugins: [react()],   // ✅ MUST be react()
-
-  preview: {
-    host: true,
-    port: process.env.PORT || 5173,
-    allowedHosts: "all"
-  }
-
+  plugins: [react()]
 })
